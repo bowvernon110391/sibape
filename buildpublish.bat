010b5798f5e56d742986e5e919d60d2dd01c1552
PDAD Soetta <pdad.soetta@gmail.com>
2019-10-30 10:43:08 +0700

    Update build and publish script

--- buildpublish.bat
+++ buildpublish.bat
@@ -1,3 +1,30 @@
 @echo off
-echo Compilation start!
-npm run build && echo App compiled. Publishing... && publish && echo App Published.
+(
+    echo =================================================================
+    echo Compilation start!
+    echo =================================================================
+    npm run build && goto publish
+    goto fail
+
+    :publish
+    echo =================================================================
+    echo Compilation success!
+    echo Publishing app...
+    echo =================================================================
+    publish
+    if errorlevel 0 goto success
+    echo Publish failed.
+    goto fail
+
+    :success
+    echo =================================================================
+    echo App published!
+    echo =================================================================
+    goto quit
+
+    :fail
+    echo Process exited with code %ERRORLEVEL%
+
+    :quit
+    echo Script ended.
+)
