e9f7a93d025e54580f4301e73034b855bdf7d6cf
Bow Vernon <bowvernon110391@gmail.com>
2020-02-21 16:31:38 +0700

    Add BrowseSpp, TableSpp, ViewSpp (unfinished)

---
+++ src/components/TableSpp.vue
@@ -0,0 +1,100 @@
+<template>
+    <b-table
+        responsive="sm"
+        table-class="shadow"
+        head-variant="dark"
+        bordered
+        outlined
+        striped
+        small
+        hover
+        primary-key="id"
+        :fields="fields"
+        v-bind="$attrs"
+        v-on="$listeners">
+        <template v-slot:cell(showDetail)="row">
+            <b-button size="sm" variant="dark" @click="row.toggleDetails">
+                <font-awesome-icon :icon="row.detailsShowing ? 'minus-square' : 'plus-square'">
+                </font-awesome-icon>
+            </b-button>
+        </template>
+
+        <!-- Detail Row -->
+        <template v-slot:row-details="row">
+            <!-- <card-view-cd :row="row"></card-view-cd> -->
+            <pre>{{ JSON.stringify(row.item, null, 2) }}</pre>
+        </template>
+
+        <template v-slot:cell(is_locked)="data">
+            <b-badge :variant="data.item.is_locked ? 'danger' : 'primary'">
+                {{ data.item.is_locked ? 'Locked' : 'Open' }}
+                <font-awesome-icon :icon="data.item.is_locked ? 'lock' : 'lock-open'">
+                </font-awesome-icon>
+            </b-badge>
+        </template>
+
+        <template v-slot:cell(action)="data">
+            <div class="text-center">
+                <!-- Edit Spp -->
+                <b-button variant="primary" size="sm" :to="`/spp/${data.item.id}`">
+                    <font-awesome-icon icon="pencil-alt">
+                    </font-awesome-icon>
+                </b-button>
+                <!-- Delete Spp -->
+                <b-button variant="danger" size="sm" :disabled="!canDelete(data.item.is_locked)"
+                    @click="onDelete(data.item.id, data.item.nomor_lengkap)">
+                    <font-awesome-icon icon="trash-alt">
+                    </font-awesome-icon>
+                </b-button>
+            </div>
+        </template>
+    </b-table>
+</template>
+
+<script>
+import userChecker from '../mixins/userChecker'
+import CardViewCd from '@/components/CardViewCd'
+
+export default {
+    inheritAttrs: false,
+    mixins: [ userChecker ],
+    components: {
+        CardViewCd
+    },
+    methods: {
+        async onDelete (id, nomor) {
+            var msg = nomor ? nomor : 'Tanpa Nomor'
+            var result = await this.$bvModal.msgBoxConfirm(`Yakin mau menghapus data SPP ini? (${msg})`, {
+                title: `Menghapus SPP #${id}`,
+                size: 'md',
+                buttonSize: 'md',
+                okVariant: 'danger',
+                okTitle: 'YES',
+                cancelTitle: 'NO',
+                footerClass: 'p-2',
+                hideHeaderClose: false,
+                centered: true
+            })
+
+            if (result) {
+                // alert("Hapuuus")
+                // emit delete event
+                this.$emit('deleteHeader', id)
+            }
+        }
+    },
+    data () {
+        return {
+            fields: [
+                { label: '', key: 'showDetail' }, 
+                'nomor_lengkap', 
+                'tgl_dok', 
+                'lokasi', 
+                'pemilik_barang',
+                { label: 'Terkunci', key: 'is_locked' },
+                'action'
+            ]
+        }
+    }
+}
+</script>
---
+++ src/components/ViewSpp.vue
@@ -0,0 +1,398 @@
+<template>
+    <div>
+        <!-- Title at the top, showing lock status too -->
+        <h4>
+            <template v-if="!isNew">
+                <span :class="[ dataSpp.is_locked ? 'bg-danger' : 'bg-primary', 'text-light', 'p-2', 'shadow', 'border', 'border-dark', 'rounded']">SPP #{{ id }} <b-badge variant="light"><font-awesome-icon :icon="dataSpp.is_locked ? 'lock' : 'lock-open'"></font-awesome-icon> {{ dataSpp.is_locked ? "LOCKED" : "OPEN" }}</b-badge></span>
+            </template>
+            <template v-else>
+                Input SPP Baru
+            </template>
+        </h4>
+        <!-- Tombol Penyelesaian? -->
+        <b-row class="my-2" v-if="id != 'new'">
+            <b-col>
+                <div class="text-right" ref="btnCetak">
+                    <b-button-group size="sm" class="shadow">
+                        <!-- apabila dijadikan impor sementara -->
+                        <b-button variant="dark">
+                            <font-awesome-icon icon="print">
+                            </font-awesome-icon>
+                            Cetak
+                        </b-button>
+                    </b-button-group>
+
+                </div>
+            </b-col>
+        </b-row>
+
+        <!-- Nomor & Tgl Dokumen + Alamat -->
+        <b-row>
+            <!-- 1st col, nomor & tgl dokumen -->
+            <b-col md="6">
+                <b-form-group label="Nomor &amp; Tgl SPP">
+                    <b-input-group>
+                        <b-form-input class="rounded-left flex-grow-1" v-model="dataSpp.nomor_lengkap" type="text" disabled></b-form-input>
+                        <template v-slot:append>
+                            <datepicker class="rounded-right" id="tgl_dok" v-model="dataSpp.tgl_dok" :disabled="disableInput" style="max-width: 150px"></datepicker>
+                        </template>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
+
+            <!-- 2nd col, jenis identitas -->
+            <b-col md="6">
+                <b-form-group label="Alamat" label-for="alamat">
+                    <b-form-textarea :disabled="disableInput">
+                    </b-form-textarea>
+                </b-form-group>
+            </b-col>
+        </b-row>
+
+        <!-- Penumpang + No Identitas -->
+        <b-row>
+            <b-col md="6">
+                <b-form-group label="Penumpang" label-for="penumpang">
+                    <select-penumpang-2 
+                        v-model="dataSpp.cd.data.penumpang.data.id" 
+                        id="penumpang" 
+                        :disabled="disableInput" 
+                        :initial-options="dataSpp.cd.data.penumpang.data" 
+                        :search-on-empty="isNew"
+                        @penumpangChange="penumpangChange"></select-penumpang-2>
+                </b-form-group>
+            </b-col>
+            <b-col md="6">
+                <b-form-group label="Asal/Country of origin" label-for="negara-asal">
+                    <b-form-input 
+                        v-model="dataBpj.no_identitas" 
+                        type="text" 
+                        id="negara-asal" 
+                        :disabled="disableInput"></b-form-input>
+                </b-form-group>
+            </b-col>
+        </b-row>
+
+        <!-- Alamat penumpang -->
+        <b-row>
+            <b-col md="6" order-md="6">
+                <b-form-group label="Alamat" label-for="alamat">
+                    <b-form-textarea
+                        id="alamat"
+                        v-model="dataBpj.alamat"
+                        :disabled="disableInput">
+                    </b-form-textarea>
+                </b-form-group>
+            </b-col>
+            <!-- Bentuk Jaminan -->     
+            <b-col md="6" order-md="0">
+                           
+                <b-form-group label="Bentuk Jaminan" label-for="bentuk_jaminan">
+                    <b-form-radio-group 
+                        id="bentuk_jaminan" 
+                        v-model="dataBpj.bentuk_jaminan"
+                        :disabled="disableInput">
+                        <b-form-radio value="TUNAI">TUNAI</b-form-radio>
+                        <b-form-radio value="BANK">BANK</b-form-radio>
+                        <b-form-radio value="CUSTOMS_BOND"><em>Customs Bond</em></b-form-radio>
+                        <b-form-radio :checked="true">Lainnya</b-form-radio>
+                    </b-form-radio-group>
+                </b-form-group>
+
+                
+                <b-form-input 
+                    v-show="jaminanLainnya" 
+                    v-model="dataBpj.bentuk_jaminan"
+                    :disabled="disableInput"></b-form-input>
+
+            </b-col>
+        </b-row>
+
+        <!-- Bentuk Jaminan + jumlah-->
+        <b-row>        
+            <!-- nomor + tgl jaminan -->
+            <b-col md="4">
+                <b-form-group 
+                    label="Nomor Jaminan" 
+                    label-for="nomor-jaminan"
+                    description="Untuk Jaminan Tunai penomoran otomatis">
+                    <b-form-input
+                        id="nomor-jaminan"
+                        v-model="dataBpj.nomor_jaminan"
+                        :disabled="disableInput || dataBpj.bentuk_jaminan == 'TUNAI'"></b-form-input>
+                </b-form-group>
+            </b-col>
+
+            <!-- Tanggal jaminan -->
+            <b-col md="2">
+                <b-form-group label="Tanggal Jaminan" label-for="tanggal-jaminan">
+                    <datepicker
+                        id="tanggal-jaminan"
+                        v-model="dataBpj.tanggal_jaminan"
+                        :disabled="disableInput">
+                    </datepicker>
+                </b-form-group>
+            </b-col>
+
+            <!-- Jumlah -->
+            <b-col md="4">
+                <b-form-group label="Jumlah (Rp)." label-for="jumlah">
+                    <b-form-input
+                        v-model="dataBpj.jumlah"
+                        :disabled="disableInput"
+
+                        class="text-right"></b-form-input>
+                </b-form-group>
+            </b-col>
+
+            <!-- Tgl Jatuh Tempo -->
+            <b-col md="2">
+                <b-form-group label="Tgl Jatuh Tempo" label-for="tanggal-jatuh-tempo">
+                    <datepicker 
+                        id="tanggal-jatuh-tempo"
+                        v-model="dataBpj.tanggal_jatuh_tempo"
+                        :disabled="disableInput">
+                    </datepicker>
+                </b-form-group>
+            </b-col>
+        </b-row>        
+
+        <!-- nomor jaminan + tgl jaminan -->
+        <b-row>
+            
+        </b-row>
+
+        <!-- Penjamin + Alamat -->
+        <b-row>
+            <!-- penjamin -->
+            <b-col md="6">
+                <b-form-group label="Penjamin" label-for="penjamin">
+                    <b-form-input
+                        v-model="dataBpj.penjamin"
+                        :disabled="disableInput"
+                        ></b-form-input>
+                </b-form-group>
+            </b-col>
+
+            <!-- alamat penjamin -->
+            <b-col md="6">
+                <b-form-group label="Alamat Penjamin" label-for="alamat-penjamin">
+                    <b-form-textarea
+                        v-model="dataBpj.alamat_penjamin"
+                        :disabled="disableInput"
+                        ></b-form-textarea>
+                </b-form-group>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <!-- catatan -->
+            <b-col md="6">
+                <b-form-group label="Catatan" label-for="catatan" description="Bisa juga direkam saat digunakan (utk pelunasan dokumen)">
+                    <b-form-textarea
+                        v-model="dataBpj.catatan"
+                        :disabled="disableInput"
+                        ></b-form-textarea>
+                </b-form-group>
+            </b-col>
+        </b-row>
+
+        <!-- Simpan -->
+        <b-row class="mt-2">
+            <b-col>
+                <b-button @click="onSave" class="float-right shadow" variant="primary" :disabled="disableInput">
+                    <font-awesome-icon icon="save"></font-awesome-icon> Simpan
+                </b-button>
+            </b-col>
+        </b-row>
+
+        <!-- <hr>
+
+        <pre>{{ dataBpj }}</pre> -->
+    </div>
+</template>
+
+<script>
+// mixins
+import axiosErrorHandler from '../mixins/axiosErrorHandler'
+import userChecker from '../mixins/userChecker'
+// components
+import SelectPenumpang2 from '@/components/SelectPenumpang2'
+import SelectNegara from '@/components/SelectNegara'
+// import Datepicker from '@/components/Datepicker'
+// import PaginatedBrowser from '@/components/PaginatedBrowser'
+// import vSelect from 'vue-select'
+// import SelectPelabuhan from '@/components/SelectPelabuhan'
+// import CardViewDetailCd from '@/components/CardViewDetailCd'
+import { mapMutations, mapGetters } from 'vuex'
+// import ViewCdDetails from '@/components/ViewCdDetails'
+
+// utk menampilkan pungutan
+// import ModalViewPerhitungan from '@/components/ModalViewPerhitungan'
+
+// ndpbm (USD)
+import SelectKurs from '@/components/SelectKurs'
+
+// the default cd header
+import defaultSpp from './defaultSpp.json'
+
+// for deep copy
+const cloneDeep = require('clone-deep')
+
+export default {
+    mixins: [ axiosErrorHandler, userChecker ],
+    components: {
+        SelectKurs,
+        SelectPenumpang2,
+        SelectNegara
+    },
+    data() {
+        return {
+            dataSpp: this.defaultData()
+        }
+    },
+    computed: {
+        // pick api and lokasi from our store
+        ...mapGetters(['api', 'lokasi']),
+
+        // when to disable input?
+        disableInput () {
+           // only disable input if user can't edit
+           // and the doc is locked
+           return !this.canEdit && this.dataBpj.is_locked
+        },
+
+        // check if this is a new data
+        isNew () {
+            return this.id == 'new'
+        },
+
+        // check if jenis jaminan is lain2
+        jaminanLainnya () {
+            return this.dataBpj.bentuk_jaminan != 'TUNAI'
+                && this.dataBpj.bentuk_jaminan != 'BANK'
+                && this.dataBpj.bentuk_jaminan != 'CUSTOMS_BOND';
+        }
+    },
+    props: {
+        id: [Number,String]
+    },
+    methods: {
+        ...mapMutations(['setBusyState']),
+        defaultData: function() {
+            return cloneDeep({
+                ...defaultSpp,
+                lokasi: this.lokasi
+            })
+        },
+
+        displayJumlah (value, event) {
+            return this.$options.filters.formatCurrency(value)
+        },
+
+        penumpangChange(data) {
+            if (data) {
+                console.log('penumpang changed:')
+                console.log(data)
+
+                if (this.dataBpj.jenis_identitas == 'PASPOR' && !this.dataBpj.no_identitas) {
+                    this.dataBpj.no_identitas = data.no_paspor // + ' <---> ' + this.dataBpj.penumpang_id
+                }
+            }
+        },
+
+        loadSppData (id) {
+            if (id == 'new') {
+                console.log("Input SPP Baru di sini!")
+                // 1st, init with empty data
+                this.dataSpp = this.defaultData()
+                // 2nd, 
+
+                return
+            }
+            // 
+            this.setBusyState(true)
+            // api
+            const vm = this
+            this.api.getSppById(id)
+                .then(e => {
+                    vm.setBusyState(false)
+                    vm.dataSpp = e.data.data
+                })
+                .catch(e => {
+                    vm.setBusyState(false)
+                    vm.handleError(e)
+
+                    // kalo gk ketemu, replace ke new
+                    vm.$router.push({
+                       path: '/spp'
+                    })
+                })
+        },
+
+        // when save button clicked
+        onSave () {
+           this.setBusyState(true)
+           const vm = this
+           // kalo id dataBpj null, berarti buat baru
+           if (!this.dataBpj.id) {
+              this.api.createBpj(this.dataBpj)
+               .then(e => {
+                  vm.setBusyState(false)
+                  var newId = e.data.id
+                  vm.showToast('Data SPP Tersimpan!', `Data Bpj tersimpan dengan id #${newId}`, "success")
+                  // re route
+                  this.$router.replace({
+                     name: 'ViewSpp',
+                     params: {
+                        id: newId
+                     }
+                  })
+               })  
+               .catch(e => {
+                  vm.setBusyState(false)
+                  vm.handleError(e)
+               })
+           } else {
+              // klo udh ada id, berarti simpan
+              this.api.updateBpj(this.id, this.dataBpj)
+               .then(e => {
+                  vm.setBusyState(false)
+                  vm.showToast('Data SPP terupdate!', `Berhasil mengupdate data BPJ #${vm.id}`, "success")
+                  // reload biar sinkron?
+                  vm.loadBpjData(vm.id)
+               })
+               .catch(e => {
+                  vm.setBusyState(false)
+                  vm.handleError(e)
+               })
+           }
+        }
+    },
+    created () {
+        // this.setBusyState(true)
+        this.loadSppData(this.id)
+    },
+    mounted () {
+        // this.setBusyState(false)
+        // // console.log(this.$route);
+        // console.log('Data CD:')
+        // console.log(this.dataBpj)
+        // console.log(this.id)
+        // console.log(Number(this.id))
+    },
+    watch: {
+        id: {
+            immediate: false,
+            handler (newVal) {
+                this.loadSppData(newVal)
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+
+</style>
