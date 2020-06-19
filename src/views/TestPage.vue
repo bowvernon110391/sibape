e59ad6a5c458b82c95fcdb350585a3282eb4884d
Bow Vernon <bowvernon110391@gmail.com>
2020-06-20 00:56:48 +0700

    Add TestPage component for TESTING

---
+++ src/views/TestPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>Just some testin done here dude.</p>
+        <p><select-pemeriksa v-model="pemeriksa_id"></select-pemeriksa></p>
+        <div>
+        </div>
+        <p>ID Pemeriksa: {{ pemeriksa_id }}</p>
+        <pre>{{ pemeriksa_id }}</pre>
+    </div>
+</template>
+
+<script>
+import SelectPemeriksa from '@/components/SelectPemeriksa'
+
+export default {
+    components: {
+        SelectPemeriksa
+    },
+
+    data () {
+        return {
+            pemeriksa_id: 296
+        }
+    }
+}
+</script>
