2d709990c942bd321e656aba56ea20b00079e2fe
Bow Vernon <bowvernon110391@gmail.com>
2020-06-20 00:00:18 +0700

    Add Forbidden page for users with no access to the app

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,8 @@ import ViewCd from '@/views/ViewCd'
 import DummyView from '@/views/DummyView'
 import LoginPage from '@/views/LoginPage'
 import PageNotFound from '@/views/PageNotFound'
+import Forbidden from '@/views/Forbidden'
+
 import BrowsePenumpang from '@/views/BrowsePenumpang'
 import BrowseKurs from '@/views/BrowseKurs'
 // import PrintSppbmcp from '@/components/PrintSppbmcp'
@@ -35,191 +37,196 @@ Vue.use(Router)
 
 const router = new Router({
   routes: [
-      // specific path put first so it can resolve
-      /* {
-         path: '/print-sppbmcp',
-         component: PrintSppbmcp
-      }, */
-      { 
-        /* root path */
-         path: '/',
-         component: MainApp,
-         meta: {
-            breadcrumb: '🏠Home',
-            authRequired: true
-         },
-         children: [
-            { /* /home */
-               path: '',
-               name: 'HomePage',
-               component: HomePage,
-               meta: {
-                  title: 'Home Page'
-               }
-            },
-            { /* /cd */
-               path: 'cd',
-               component: DummyView,
-               meta: {
-                  breadcrumb: '🔍Browse CD'
-               },
-               children: [
-                  { // /cd/{id}
-                     path: ':id',
-                     name: 'ViewCd',
-                     component: ViewCd,
-                     props: true,
-                     meta: {
-                        title: 'Detail CD',
-                        breadcrumb: '💰Lihat Detail CD'
-                     }
-                  },
-                  { //
-                     path: '',
-                     name: 'BrowseCd',
-                     meta: {
-                        title: 'Browse Customs Declaration'
-                     },
-                     component: BrowseCd
-                  }
-               ]
-            },
-            /* penumpang */
-            {
-              path: 'penumpang',
-              name: 'BrowsePenumpang',
+    // specific path put first so it can resolve
+    /* {
+       path: '/print-sppbmcp',
+       component: PrintSppbmcp
+    }, */
+    {
+      /* root path */
+      path: '/',
+      component: MainApp,
+      meta: {
+        breadcrumb: '🏠Home',
+        authRequired: true
+      },
+      children: [
+        { /* /home */
+          path: '',
+          name: 'HomePage',
+          component: HomePage,
+          meta: {
+            title: 'Home Page'
+          }
+        },
+        { /* /cd */
+          path: 'cd',
+          component: DummyView,
+          meta: {
+            breadcrumb: '🔍Browse CD'
+          },
+          children: [
+            { // /cd/{id}
+              path: ':id',
+              name: 'ViewCd',
+              component: ViewCd,
+              props: true,
               meta: {
-                breadcrumb: '🔍Browse Penumpang',
-                title: 'Browse Data Penumpang'
-              },
-              component: BrowsePenumpang
+                title: 'Detail CD',
+                breadcrumb: '💰Lihat Detail CD'
+              }
             },
-            /* kurs */
-            {
-              path: 'kurs',
-              component: DummyView,
+            { //
+              path: '',
+              name: 'BrowseCd',
               meta: {
-                breadcrumb: '🔍Browse Data Kurs',
-                title: 'Browse Data Kurs'
+                title: 'Browse Customs Declaration'
               },
-              component: BrowseKurs
-            },
-            /* bpj */
-            {
-              path: 'bpj',
-              meta: {
-                breadcrumb: '🔍Browse BPJ'
-              },
-              component: DummyView,
-              children: [
-                // /bpj/:id (ViewBpj)
-                {
-                  path: ':id',
-                  name: 'ViewBpj',
-                  component: ViewBpj,
-                  props: true,
-                  meta: {
-                    title: 'Detail BPJ',
-                    breadcrumb: '📝Lihat Detail BPJ'
-                  }
-                },
-                // default (Browse BPJ)
-                {
-                  path: '',
-                  name: 'BrowseBpj',
-                  component: BrowseBpj,
-                  meta: {
-                    title: 'Browse BPJ'
-                  }
-                }
-              ]              
-            },
-            /* pembatalan */{
-              path: 'pembatalan',
-              name: 'BrowsePembatalan',
-              meta: {
-                breadcrumb: '🔍Browse Pembatalan',
-                title: 'Browse Surat Pembatalan'
-              },
-              component: BrowsePembatalan
-            },
-            /* spp */
-            {
-              path: 'spp',
-              meta: {
-                breadcrumb: '🔍Browse SPP'
-              },
-              component: DummyView,
-              children: [
-                {
-                  path: ':id',
-                  name: 'ViewSpp',
-                  component: ViewSpp,
-                  props: true,
-                  meta: {
-                    title: 'Detail SPP',
-                    breadcrumb: '✋Lihat Detail SPP'
-                  }
-                },
-                // default (Browse SPP)
-                {
-                  path: '',
-                  name: 'BrowseSpp',
-                  component: BrowseSpp,
-                  meta: {
-                    title: 'Browse SPP'
-                  }
-                }
-              ]
-            },
-            /* st */
-            {
-              path: 'st',
-              meta: {
-                breadcrumb: '🔍Browse ST'
-              },
-              component: DummyView,
-              children: [
-                {
-                  path: ':id',
-                  name: 'ViewSt',
-                  component: ViewSt,
-                  props: true,
-                  meta: {
-                    title: 'Detail ST',
-                    breadcrumb: '🛅Lihat Detail ST'
-                  }
-                },
-                // default (Browse SPP)
-                {
-                  path: '',
-                  name: 'BrowseSt',
-                  component: BrowseSt,
-                  meta: {
-                    title: 'Browse ST'
-                  }
-                }
-              ]
+              component: BrowseCd
             }
-         ]
-      },
-      {
-         path: '/login',
-         name: 'Login',
-         component: LoginPage
-      },
-      {
-         path: '*',
-         name: 'PageNotFound',
-         component: PageNotFound
-      }
-   ],
+          ]
+        },
+        /* penumpang */
+        {
+          path: 'penumpang',
+          name: 'BrowsePenumpang',
+          meta: {
+            breadcrumb: '🔍Browse Penumpang',
+            title: 'Browse Data Penumpang'
+          },
+          component: BrowsePenumpang
+        },
+        /* kurs */
+        {
+          path: 'kurs',
+          component: DummyView,
+          meta: {
+            breadcrumb: '🔍Browse Data Kurs',
+            title: 'Browse Data Kurs'
+          },
+          component: BrowseKurs
+        },
+        /* bpj */
+        {
+          path: 'bpj',
+          meta: {
+            breadcrumb: '🔍Browse BPJ'
+          },
+          component: DummyView,
+          children: [
+            // /bpj/:id (ViewBpj)
+            {
+              path: ':id',
+              name: 'ViewBpj',
+              component: ViewBpj,
+              props: true,
+              meta: {
+                title: 'Detail BPJ',
+                breadcrumb: '📝Lihat Detail BPJ'
+              }
+            },
+            // default (Browse BPJ)
+            {
+              path: '',
+              name: 'BrowseBpj',
+              component: BrowseBpj,
+              meta: {
+                title: 'Browse BPJ'
+              }
+            }
+          ]
+        },
+            /* pembatalan */{
+          path: 'pembatalan',
+          name: 'BrowsePembatalan',
+          meta: {
+            breadcrumb: '🔍Browse Pembatalan',
+            title: 'Browse Surat Pembatalan'
+          },
+          component: BrowsePembatalan
+        },
+        /* spp */
+        {
+          path: 'spp',
+          meta: {
+            breadcrumb: '🔍Browse SPP'
+          },
+          component: DummyView,
+          children: [
+            {
+              path: ':id',
+              name: 'ViewSpp',
+              component: ViewSpp,
+              props: true,
+              meta: {
+                title: 'Detail SPP',
+                breadcrumb: '✋Lihat Detail SPP'
+              }
+            },
+            // default (Browse SPP)
+            {
+              path: '',
+              name: 'BrowseSpp',
+              component: BrowseSpp,
+              meta: {
+                title: 'Browse SPP'
+              }
+            }
+          ]
+        },
+        /* st */
+        {
+          path: 'st',
+          meta: {
+            breadcrumb: '🔍Browse ST'
+          },
+          component: DummyView,
+          children: [
+            {
+              path: ':id',
+              name: 'ViewSt',
+              component: ViewSt,
+              props: true,
+              meta: {
+                title: 'Detail ST',
+                breadcrumb: '🛅Lihat Detail ST'
+              }
+            },
+            // default (Browse SPP)
+            {
+              path: '',
+              name: 'BrowseSt',
+              component: BrowseSt,
+              meta: {
+                title: 'Browse ST'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginPage
+    },
+    {
+      path: '/forbidden',
+      name: 'Forbidden',
+      component: Forbidden
+    },
+    {
+      path: '*',
+      name: 'PageNotFound',
+      component: PageNotFound
+    }
+  ],
 
   //  mode: 'history'
 })
 
 // set navigation guards here
-router.beforeEach(async (to, from, next) => {  
+router.beforeEach(async (to, from, next) => {
   console.log('-->>')
   console.log(to)
 
@@ -229,17 +236,17 @@ router.beforeEach(async (to, from, next) => {
   // bypass authentication if we're in dev mode
   // also, set default userInfo
   if (process.env.NODE_ENV != 'production') {
-     console.log("Not in production. Bypassing login and authentication and using default user")
+    console.log("Not in production. Bypassing login and authentication and using default user")
 
-     var mockRoles = {
-       token_pdtt: ['PDTT'],
-       token_admin: ['CONSOLE','KASI']
-     }
+    var mockRoles = {
+      token_pdtt: ['PDTT'],
+      token_admin: ['CONSOLE', 'KASI']
+    }
 
-     var lokasiDemo = process.env.MOCK_LOKASI || "KANTOR"
-     var tokenDemo = process.env.MOCK_TOKEN || 'token_admin'
+    var lokasiDemo = process.env.MOCK_LOKASI || "KANTOR"
+    var tokenDemo = process.env.MOCK_TOKEN || 'token_admin'
 
-     var userDemo = {
+    var userDemo = {
       "user_id": "666",
       "username": "user.demo",
       "name": "User Demo",
@@ -273,7 +280,7 @@ router.beforeEach(async (to, from, next) => {
         }
       }
     }
-    
+
     // store default data
     store.commit('setUserInfo', userDemo)
     store.commit('setLokasi', lokasiDemo)
@@ -286,9 +293,9 @@ router.beforeEach(async (to, from, next) => {
   console.log(store.state.userInfo)
   if (authRequired /* && !store.state.userInfo */) {
     // perhaps state.userInfo is reset, but our token is still valid?
-   //  var tokenData = document.cookie.split("=")
-    var cookies = cookie.parse(document.cookie) 
-    console.log(cookies)     
+    //  var tokenData = document.cookie.split("=")
+    var cookies = cookie.parse(document.cookie)
+    console.log(cookies)
     var token = cookies.sso_token_5
 
     var tries = 0
@@ -299,13 +306,13 @@ router.beforeEach(async (to, from, next) => {
       cookies = cookie.parse(document.cookie)
       token = cookies.sso_token_5
     }
-    
+
     // use token from .env if there's one
     if (process.env.VUE_APP_TOKEN) {
-       console.log('ENV token exists! using it instead...')
-       token = process.env.VUE_APP_TOKEN
+      console.log('ENV token exists! using it instead...')
+      token = process.env.VUE_APP_TOKEN
     }
-    
+
     console.log("Token Cookie:")
     console.log(token)
     // show loading screen
@@ -323,33 +330,39 @@ router.beforeEach(async (to, from, next) => {
         url: '/static/sso/api.php?command=getUserInfo'
       }) */
       store.getters.sso.getUserInfo()
-      .then((e) => {
-        store.commit('setBusyState', false)
-        // we got user info! but is it empty?
-        if (!e.data) {
-          // empty!! We're not logged in
+        .then((e) => {
+          store.commit('setBusyState', false)
+          // we got user info! but is it empty?
+          if (!e.data) {
+            // empty!! We're not logged in
+            next('/login')
+          } else {
+            // data is valid. store it to store
+            store.commit('setUserInfo', e.data)
+            // also store token
+            store.commit('setToken', token)
+            // log store data?
+            console.log('store now: ')
+            console.log(store)
+            // redirect to whatever page we were accessing
+            if (store.getters.roles !== false) {
+              console.log('Has valid roles. Go on...')
+              next()
+            } else {
+              console.log('FORBIDDEN!!')
+              next('/forbidden')
+            }
+          }
+        })
+        .catch((e) => {
+          store.commit('setBusyState', false)
+          // obviously we're in deep trouble
+          // either the token is not-attached to sso session
+          // or sso server is down. For now, redirect to
+          // login
           next('/login')
-        } else {
-          // data is valid. store it to store
-          store.commit('setUserInfo', e.data)
-          // also store token
-          store.commit('setToken', token)
-          // log store data?
-          console.log('store now: ')
-          console.log(store)
-          // redirect to whatever page we were accessing
-          next()
-        }
-      })
-      .catch((e) => {
-        store.commit('setBusyState', false)
-        // obviously we're in deep trouble
-        // either the token is not-attached to sso session
-        // or sso server is down. For now, redirect to
-        // login
-        next('/login')
-      })
-      
+        })
+
     }
   } else {
     store.commit('setBusyState', false)
---
+++ src/views/Forbidden.vue
@@ -0,0 +1,16 @@
+<template>
+    <b-card bg-variant="danger" text-variant="light" border-variant="secondary" class="shadow text-center align-middle">
+        <b-card-title>
+            🚫 Forbidden Access 🚫
+        </b-card-title>
+        <b-card-text>
+            You have no business accessing this application. Go somewhere else, be productive.
+        </b-card-text>
+    </b-card>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
