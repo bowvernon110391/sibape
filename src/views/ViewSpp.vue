c2e7405d355f8890154b6b8cd810403736be59da
Bow Vernon <bowvernon110391@gmail.com>
2020-06-22 22:43:18 +0700

    Change ViewSpp to use broken down components

--- src/views/ViewSpp.vue
+++ src/views/ViewSpp.vue
@@ -1,337 +1,202 @@
 <template>
-    <div v-if="dataSpp.cd">
-        <!-- Title at the top, showing lock status too -->
-        <h4>
-            <template v-if="!isNew">
-                <span :class="[ dataSpp.is_locked ? 'bg-danger' : 'bg-primary', 'text-light', 'p-2', 'shadow', 'border', 'border-dark', 'rounded']">SPP #{{ id }} <b-badge variant="light"><font-awesome-icon :icon="dataSpp.is_locked ? 'lock' : 'lock-open'"></font-awesome-icon> {{ dataSpp.is_locked ? "LOCKED" : "OPEN" }}</b-badge></span>
-            </template>
-            <template v-else>
-                Input SPP Baru
-            </template>
-        </h4>
-        <!-- Tombol Penyelesaian? -->
-        <b-row class="my-2" v-if="id != 'new'">
-            <b-col>
-                <div class="text-right" ref="btnCetak">
-                    <b-button-group size="sm" class="shadow mt-2 mt-md-0">
-                        <!-- buatkan / lihat pibk -->
-                        <b-button variant="success">
-                            <font-awesome-icon icon="money-check-alt"></font-awesome-icon>
-                            <template v-if="sppHasLink('pibk')">
-                                Lihat PIBK
-                            </template>
-                            <template v-else>
-                                Buatkan PIBK
-                            </template>
-                        </b-button>
-                    </b-button-group>
+  <div v-if="dataSpp.cd">
+    <b-card no-body class="rounded shadow">
+      <!-- HEADER -->
+      <b-card-header>
+        <div class="d-sm-block d-md-flex my-2">
+          <!-- Title banner at the top -->
+          <doc-banner doctype="SPP" :data="dataSpp" :is-new="isNew" />
 
-                    <b-button-group size="sm" class="shadow mt-2 mt-md-0">
-                        <!-- apabila dijadikan impor sementara -->
-                        <b-button variant="dark" @click="printSpp">
-                            <font-awesome-icon icon="print">
-                            </font-awesome-icon>
-                            Cetak
-                        </b-button>
-                    </b-button-group>
+          <!-- show controls if it's not a new one -->
+          <template v-if="!hideControls && !isNew">
+            <!-- tombol controls -->
+            <spp-controls :data="dataSpp" ref="tombolPenyelesaian" @printSpp="printSpp" />
+          </template>
+        </div>
+      </b-card-header>
 
-                </div>
-            </b-col>
-        </b-row>
 
-        <!-- Nomor & Tgl Dokumen + Alamat -->
-        <b-row>
-            <!-- 1st col, nomor & tgl dokumen -->
-            <b-col md="6">
-                <b-form-group label="Nomor &amp; Tgl SPP">
-                    <b-input-group>
-                        <b-form-input class="rounded-left flex-grow-1" v-model="dataSpp.nomor_lengkap" type="text" disabled></b-form-input>
-                        <template v-slot:append>
-                            <datepicker class="rounded-right" id="tgl_dok" v-model="dataSpp.tgl_dok" :disabled="disableInput" style="max-width: 150px"></datepicker>
-                        </template>
-                    </b-input-group>
-                </b-form-group>
-            </b-col>
+      <!-- Tabs -->
+      <b-tabs card lazy v-model="tabId">
+        <!-- Header -->
+        <b-tab title="Header" active>
+          <spp-contents :data="dataSpp" :disabled="disableInput" :is-new="isNew" />
+        </b-tab>
 
-            <!-- Penumpang -->
-            <b-col md="6">
-                <b-form-group label="Penumpang" label-for="penumpang">
-                    <select-penumpang-2 
-                        v-if="dataSpp.cd"
-                        v-model="dataSpp.cd.data.penumpang.data.id" 
-                        id="penumpang" 
-                        :disabled="disableInput" 
-                        :initial-options="dataSpp.cd.data.penumpang.data" 
-                        :search-on-empty="isNew"></select-penumpang-2>
-                </b-form-group>
-            </b-col>
-        </b-row>
+        <!-- Barang baca dari cd -->
+        <b-tab title="Barang" v-if="!isNew">
+          <view-cd-details
+            :cd-id="dataSpp.cd.data.id"
+            :disabled="disableInput"
+            hide-satuan
+            hide-netto
+          ></view-cd-details>
+        </b-tab>
+      </b-tabs>
 
-        <!-- Penumpang + No Identitas -->
-        <b-row>
-            <!-- 2nd col, jenis identitas -->
-            <b-col md="6">
-                <b-form-group label="Alamat" label-for="alamat">
-                    <b-form-textarea 
-                        :disabled="disableInput"
-                        v-model="dataSpp.cd.data.alamat">
-                    </b-form-textarea>
-                </b-form-group>
-            </b-col>
-            
-            <b-col md="6">
-                <b-form-group label="Asal/Country of origin" label-for="negara-asal">
-                    <!-- <b-form-input 
-                        v-model="dataSpp.negara_asal.data.kode" 
-                        type="text" 
-                        id="negara-asal" 
-                        :disabled="disableInput"></b-form-input> -->
-                    <select-negara
-                        v-model="dataSpp.negara_asal.data.kode"
-                        id="negara-asal"
-                        :disabled="disableInput"></select-negara>
-                </b-form-group>
-            </b-col>
-        </b-row>
+      <!-- FOOTER (only if needed) -->
+      <b-card-footer v-if="tabId == 0">
+        <div>
+          <b-button variant="primary" :disabled="disableInput">
+            <font-awesome-icon icon="save" /> Simpan
+          </b-button>
+        </div>
+      </b-card-footer>
+    </b-card>
 
-        <!-- No Flight + Keterangan? -->
-        <b-row>
-            <!-- Nomor Flight -->
-            <b-col md="6">
-                <b-form-group label="Flight/Voyage No.">
-                    <b-input-group>
-                        <b-form-input
-                            v-model="dataSpp.cd.data.no_flight"
-                            :disabled="disableInput"/>
-                        <template v-slot:append>
-                            <select-airline 
-                                :tabindex="9999"
-                                style="margin: 0 2px; width: 100%"
-                                :disabled="disableInput"
-                                v-model="dataSpp.cd.data.kd_airline"/>
-                        </template>
-                    </b-input-group>
-                </b-form-group>
-
-            </b-col>
-
-            <!-- Keterangan -->
-            <b-col md="6">
-                <b-form-group label="Keterangan" label-for="keterangan">
-                    <b-form-textarea
-                        id="keterangan"
-                        v-model="dataSpp.keterangan"
-                        :disabled="disableInput">
-                    </b-form-textarea>
-                </b-form-group>
-            </b-col>
-        </b-row>
-
-        <!-- Simpan -->
-        <b-row class="mt-2">
-            <b-col>
-                <b-button @click="onSave" class="float-right shadow" variant="primary" :disabled="disableInput">
-                    <font-awesome-icon icon="save"></font-awesome-icon> Simpan
-                </b-button>
-            </b-col>
-        </b-row>
-
-        <!-- Paginated for detail data -->
-        <template v-if="!isNew">
-            <hr>
-            <view-cd-details 
-                :cd-id="dataSpp.cd.data.id"
-                :disabled="disableInput"
-                hide-satuan
-                hide-netto>
-            </view-cd-details>
-        </template>
-
-        <!-- PRINT MODAL -->
-        <modal-view-pdf
-            v-model="viewPrintDialog"
-            :url="pdfUrl"
-            :alt-filename="altFilename"
-            ></modal-view-pdf>
-    </div>
-
+    <!-- PRINT MODAL -->
+    <modal-view-pdf v-model="viewPrintDialog" :url="pdfUrl" :alt-filename="altFilename"></modal-view-pdf>
+  </div>
 </template>
 
 <script>
 // mixins
-import axiosErrorHandler from '../mixins/axiosErrorHandler'
-import userChecker from '../mixins/userChecker'
+import axiosErrorHandler from "../mixins/axiosErrorHandler";
+import userChecker from "../mixins/userChecker";
+
 // components
-import SelectPenumpang2 from '@/components/SelectPenumpang2'
-import SelectNegara from '@/components/SelectNegara'
-import SelectAirline from '@/components/SelectAirline'
-import Datepicker from '@/components/Datepicker'
-// import PaginatedBrowser from '@/components/PaginatedBrowser'
-// import vSelect from 'vue-select'
-// import SelectPelabuhan from '@/components/SelectPelabuhan'
-// import CardViewDetailCd from '@/components/CardViewDetailCd'
-import { mapMutations, mapGetters } from 'vuex'
-import ViewCdDetails from '@/views/ViewCdDetails'
+import { mapMutations, mapGetters } from "vuex";
+import ViewCdDetails from "@/views/ViewCdDetails";
 
 // utk menampilkan pdf
-import ModalViewPdf from '@/components/ModalViewPdf'
+import ModalViewPdf from "@/components/ModalViewPdf";
 
-// ndpbm (USD)
-import SelectKurs from '@/components/SelectKurs'
+// banner
+import DocBanner from "@/components/DocBanner";
+
+// controls
+import SppControls from "@/components/SppControls";
+
+// contents
+import SppContents from "@/components/SppContents";
 
 // the default cd header
-import defaultSpp from '@/defaults/defaultSpp'
+import defaultSpp from "@/defaults/defaultSpp";
 
 // for deep copy
-const cloneDeep = require('clone-deep')
+const cloneDeep = require("clone-deep");
 
 export default {
-    mixins: [ axiosErrorHandler, userChecker ],
-    components: {
-        SelectKurs,
-        SelectPenumpang2,
-        SelectNegara,
-        ViewCdDetails,
-        SelectAirline,
-        Datepicker,
-        ModalViewPdf
+  mixins: [axiosErrorHandler, userChecker],
+  components: {
+    ViewCdDetails,
+    ModalViewPdf,
+    DocBanner,
+    SppControls,
+    SppContents
+  },
+  data() {
+    return {
+      dataSpp: this.defaultData(),
+
+      // for printing
+      viewPrintDialog: false,
+      pdfUrl: null,
+      altFilename: "SPP",
+
+      // tab id
+      tabId : 0
+    };
+  },
+  computed: {
+    // pick api and lokasi from our store
+    ...mapGetters(["api", "lokasi"]),
+
+    // when to disable input?
+    disableInput() {
+      // only disable input if user can't edit
+      // and the doc is locked
+      return !this.canEdit && this.dataSpp.is_locked;
     },
-    data() {
-        return {
-            dataSpp: this.defaultData(),
 
-            // for printing
-            viewPrintDialog: false,
-            pdfUrl: null,
-            altFilename: 'SPP'
-        }
-    },
-    computed: {
-        // pick api and lokasi from our store
-        ...mapGetters(['api', 'lokasi']),
-
-        // when to disable input?
-        disableInput () {
-           // only disable input if user can't edit
-           // and the doc is locked
-           return !this.canEdit && this.dataSpp.is_locked
-        },
-
-        // check if this is a new data
-        isNew () {
-            return this.id == 'new'
-        }
-    },
-    props: {
-        id: [Number,String],
-
-        hideControls: {
-            type: Boolean,
-            default: false
-        }
-    },
-    methods: {
-        ...mapMutations(['setBusyState']),
-        defaultData: function() {
-            return cloneDeep({
-                ...defaultSpp,
-                lokasi: this.lokasi
-            })
-        },
-
-        displayJumlah (value, event) {
-            return this.$options.filters.formatCurrency(value)
-        },
-
-        loadSppData (id) {
-            if (id == 'new') {
-                console.log("Input SPP Baru di sini!")
-                // 1st, init with empty data
-                this.dataSpp = this.defaultData()
-                // 2nd, 
-
-                return
-            }
-            // 
-            this.setBusyState(true)
-            // api
-            const vm = this
-            this.api.getSppById(id)
-                .then(e => {
-                    vm.setBusyState(false)
-                    vm.dataSpp = e.data.data
-                })
-                .catch(e => {
-                    vm.setBusyState(false)
-                    vm.handleError(e)
-
-                    // kalo gk ketemu, replace ke new
-                    vm.$router.push({
-                       path: '/spp'
-                    })
-                })
-        },
-
-        // when save button clicked
-        onSave () {
-        },
-
-        // when print button clicked
-        printSpp () {
-            // set data
-            this.pdfUrl = this.api.generatePdfUrl('spp', this.id)
-            this.viewPrintDialog = true
-            this.altFilename = `spp-${this.id}`
-        },
-
-        // check if cd has some related documents
-        sppHasLink: function (rel) {
-            if (this.dataSpp.links) {
-                // check by filtering it
-                return this.dataSpp.links.filter(e => e.rel == rel).length > 0
-            }
-            return false
-        },
-
-        // grab link detail
-        sppGetLinkDetails: function (rel) {
-            if (this.dataSpp.links) {
-                var filtered = this.dataSpp.links.filter(e => e.rel == rel)
-
-                if (filtered.length) {
-                    var link = filtered[0]
-                    return {
-                        doctype: link.rel,
-                        id: link.uri.match(/^\/.+\/(\d+)$/i)[1]
-                    }
-                }
-            }
-            return false
-        },
-    },
-    created () {
-        // this.setBusyState(true)
-        this.loadSppData(this.id)
-    },
-    mounted () {
-        // this.setBusyState(false)
-        // // console.log(this.$route);
-        // console.log('Data CD:')
-        // console.log(this.id)
-        // console.log(Number(this.id))
-    },
-    watch: {
-        id: {
-            immediate: false,
-            handler (newVal) {
-                this.loadSppData(newVal)
-            }
-        }
+    // check if this is a new data
+    isNew() {
+      return this.id == "new";
     }
-}
+  },
+  props: {
+    id: [Number, String],
+
+    hideControls: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    ...mapMutations(["setBusyState"]),
+    defaultData: function() {
+      return cloneDeep({
+        ...defaultSpp,
+        lokasi: this.lokasi
+      });
+    },
+
+    displayJumlah(value, event) {
+      return this.$options.filters.formatCurrency(value);
+    },
+
+    loadSppData(id) {
+      if (id == "new") {
+        console.log("Input SPP Baru di sini!");
+        // 1st, init with empty data
+        this.dataSpp = this.defaultData();
+        // 2nd,
+
+        return;
+      }
+      //
+      this.setBusyState(true);
+      // api
+      const vm = this;
+      this.api
+        .getSppById(id)
+        .then(e => {
+          vm.setBusyState(false);
+          vm.dataSpp = e.data.data;
+        })
+        .catch(e => {
+          vm.setBusyState(false);
+          vm.handleError(e);
+
+          // kalo gk ketemu, replace ke new
+          vm.$router.push({
+            path: "/spp"
+          });
+        });
+    },
+
+    // when save button clicked
+    onSave() {},
+
+    // when print button clicked
+    printSpp() {
+      // set data
+      this.pdfUrl = this.api.generatePdfUrl("spp", this.id);
+      this.viewPrintDialog = true;
+      this.altFilename = `spp-${this.id}`;
+    }
+  },
+  created() {
+    // this.setBusyState(true)
+    this.loadSppData(this.id);
+  },
+  mounted() {
+    // this.setBusyState(false)
+    // // console.log(this.$route);
+    // console.log('Data CD:')
+    // console.log(this.id)
+    // console.log(Number(this.id))
+  },
+  watch: {
+    id: {
+      immediate: false,
+      handler(newVal) {
+        this.loadSppData(newVal);
+      }
+    }
+  }
+};
 </script>
 
 <style>
-
-
 </style>
