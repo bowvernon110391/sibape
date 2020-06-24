281d431538ea3f6aeb664b075dc3abd77d598e98
Bow Vernon <bowvernon110391@gmail.com>
2020-06-24 12:06:02 +0700

    Fix Select Pemeriksa syncing status

--- src/components/SelectPemeriksa.vue
+++ src/components/SelectPemeriksa.vue
@@ -1,75 +1,76 @@
 <template>
-    <api-select 
-        v-bind="$attrs" 
-        v-on="$listeners"
+  <api-select
+    v-bind="$attrs"
+    v-on="$listeners"
+    :reduce="e => e.user_id"
+    :get-option-key="e => e.user_id"
+    label="name"
+    :search-callback="searchPemeriksa"
+    :sync-callback="syncPemeriksa"
+    :search-on-empty="true"
+  >
+    <!-- Option Slot -->
+    <template #option="opt">
+      <!-- spawn some divs? -->
+      <div>
+        <strong>{{ opt.name }}</strong>
+      </div>
+      <div>NIP {{ opt.nip }}</div>
+      <div>{{ opt.pangkat }}</div>
+    </template>
 
-        :reduce="e => e.user_id"
-        :get-option-key="e => e.user_id"
-        label="name"
-        :search-callback="searchPemeriksa"
-        :sync-callback="syncPemeriksa"
-
-        :search-on-empty="true"
-        >
-        <!-- Option Slot -->
-        <template #option="opt">
-            <!-- spawn some divs? -->
-            <div>
-                <strong>{{ opt.name }}</strong>
-            </div>
-            <div>
-                NIP {{ opt.nip }}
-            </div>
-            <div>
-                {{ opt.pangkat }}
-            </div>
+    <!-- Selected option slot -->
+    <template #selected-option="opt">
+      <div>
+        <template v-if="!opt.nip">
+          <strong><em>Synchronizing...</em></strong>
         </template>
-
-        <!-- Selected option slot -->
-        <template #selected-option="opt">
-            <div>
-                <strong>{{ opt.name }}</strong> - (<em>{{ opt.nip }}</em>)
-            </div>
+        <template v-else>
+          <strong>{{ opt.name }}</strong> - (
+          <em>{{ opt.nip }}</em>)
         </template>
-    </api-select>
+      </div>
+    </template>
+  </api-select>
 </template>
 
 <script>
-import ApiSelect from '@/components/ApiSelect'
-import { mapGetters } from 'vuex'
-import axiosErrorHandler from '../mixins/axiosErrorHandler'
+import ApiSelect from "@/components/ApiSelect";
+import { mapGetters } from "vuex";
+import axiosErrorHandler from "../mixins/axiosErrorHandler";
 
 export default {
-    inheritAttrs: false,
-    components: {
-        ApiSelect
+  inheritAttrs: false,
+  components: {
+    ApiSelect
+  },
+
+  mixins: [axiosErrorHandler],
+
+  computed: {
+    ...mapGetters(["api"])
+  },
+
+  methods: {
+    searchPemeriksa(q, spinner, vm) {
+      spinner(true);
+
+      this.api
+        .getPemeriksa({
+          q: q
+        })
+        .then(e => {
+          spinner(false);
+          vm.setOptions(e.data.data);
+        })
+        .catch(e => {
+          spinner(false);
+          this.handleError(e);
+        });
     },
-
-    mixins: [axiosErrorHandler],
-
-    computed: {
-        ...mapGetters(['api'])
-    },
-
-    methods: {
-        searchPemeriksa(q, spinner, vm) {
-            spinner(true)
-
-            this.api.getPemeriksa({
-                q: q
-            })
-            .then(e => {
-                spinner(false)
-                vm.setOptions(e.data.data)
-            })
-            .catch(e => {
-                spinner(false)
-                this.handleError(e)
-            })
-        },
-        syncPemeriksa(q, spinner, vm) {
-            this.searchPemeriksa('', spinner, vm)
-        }
+    syncPemeriksa(q, spinner, vm) {
+      this.searchPemeriksa("", spinner, vm);
     }
-}
+  }
+};
 </script>
