30bd183ebe956491502a5913677d0e2960e0fd60
Bow Vernon <bowvernon110391@gmail.com>
2020-07-07 20:02:07 +0700

    Add PagePerhitunganImpor for other imports tax and duty report...

---
+++ src/components/PagePerhitunganImpor.vue
@@ -0,0 +1,193 @@
+<template>
+<div>
+    <b-table
+        v-on="$listeners"
+        v-bind="$attrs"
+
+        head-variant="dark"
+        bordered
+        striped
+        hover
+        class="shadow"
+
+        sticky-header="300px"
+        responsive="sm"
+
+        :fields="fields"
+        :items="data.barang"
+    >
+        <!-- number -->
+        <template #cell(number)="row">
+            {{ row.index+1 }}
+        </template>
+
+        <!-- DESCRIPTION -->
+        <template #cell(uraian)="row">
+            <div class="newline">{{ row.value }}</div>
+        </template>
+
+        <!-- hs_raw_code -->
+        <template #cell(hs_raw_code)="row">
+            <div class="text-center">
+                <u>{{ row.value }}</u>
+            </div>
+            <div v-for="(tarif, kode) in row.item.tarif" :key="kode">
+                -
+                <template v-if="kode.indexOf('BM') == 0">
+                    <!-- for BM Tarif -->
+                    <strong>{{ kode }}</strong>: {{ tarif.tarif }} % ({{ tarif.jenis }})
+                </template>
+                <!-- other kind -->
+                <template v-else>
+                    <strong>{{ kode }}</strong>: {{ tarif.tarif }} %
+                </template>
+            </div>
+        </template>
+
+        <!-- quantity -->
+        <template #cell(jumlah_jenis_kemasan)="row">
+            <p class="text-center">
+                {{ row.value }}
+            </p>
+            <p class="text-center">
+                {{ row.item.jumlah_jenis_satuan }}
+            </p>
+        </template>
+
+        <!-- nilai_pabean -->
+        <template #cell(nilai_pabean)="row">
+            <div class="text-right">
+            {{ row.item.fob | formatCurrency }} + {{ row.item.insurance | formatCurrency }} + {{ row.item.freight | formatCurrency }} <br>
+            = <strong>{{ row.item.valuta }}</strong> {{ row.item.cif | formatCurrency }}<br>
+            = <strong>IDR</strong> {{ row.item.nilai_pabean | formatCurrency }}
+            </div>
+        </template>
+
+        <!-- tax and duty -->
+        <template #cell(tax_and_duty)="row">
+            <div v-for="(pungutan, id) in row.item.pungutan" :key="id+'pungutan'+pungutan.jenis_pungutan_id">
+                <strong>{{ pungutan.jenis_pungutan.kode }}</strong>: {{ pungutan.bayar | formatCurrency |displayRupiah }}
+            </div>
+        </template>
+    </b-table>
+
+    <!-- now we just list the steps of calculation -->
+    <b-card
+    header-bg-variant="light"
+    footer-bg-variant="light"
+    class="shadow"
+    >
+        <template #header>
+            <h5 class="my-0"><font-awesome-icon icon="money-check-alt"/> Duty and Tax Summary</h5>
+        </template>
+
+        <b-row v-for="(pungutan, kode) in data.pungutan.bayar" :key="kode">
+            <b-col md="8">
+                <strong>{{ dict[kode][0] }} (<em>{{ dict[kode][1] }}</em>)</strong>
+            </b-col>
+            <b-col md="4" class="text-right">
+                {{ pungutan | formatCurrency | displayRupiah }}
+            </b-col>
+        </b-row>
+        
+        <template #footer>
+        <b-row>
+            <b-col md="8" class="text-center">
+                <strong>Total Bea Masuk dan Pajak (<em>Total Duty and Tax</em>)</strong>
+            </b-col>
+            <b-col md="4" class="text-right">
+                <u><strong>{{ totalDutyAndTax | formatCurrency | displayRupiah }}</strong></u>
+            </b-col>
+        </b-row>
+        </template>
+    </b-card>
+</div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+
+    props: {
+        data: {
+            type: Object,
+            required: true
+        }
+    },
+
+    computed: {
+        totalNilaiPabean() {
+            return this.data.barang.reduce((prev, curr) => (prev + curr.nilai_pabean), 0.0)
+        },
+
+        totalDutyAndTax() {
+            var total = 0
+            for (let kode in this.data.pungutan.bayar) {
+                total += this.data.pungutan.bayar[kode]
+            }
+
+            return total
+        }
+    },
+
+    data () {
+        return {
+            fields: [
+                {
+                    label: 'No.',
+                    key: 'number'
+                },
+                {
+                    label: 'Description',
+                    key: 'uraian'
+                },
+                {
+                    label: 'HS Code and Tariffs',
+                    key: 'hs_raw_code'
+                },
+                {
+                    label: 'Quantity',
+                    key: 'jumlah_jenis_kemasan'
+                },
+                {
+                    label: 'Customs Value (CIF)',
+                    key: 'nilai_pabean'
+                },
+                {
+                    label: 'Duty and Tax',
+                    key: 'tax_and_duty'
+                }
+            ],
+
+            dict: {
+                'BM': [
+                    'Bea Masuk',
+                    'Import Duty'
+                ],
+                'BMTP': [
+                    'Bea Masuk Tindak Pengamaman',
+                    'Duty for Protection'
+                ],
+                'PPN': [
+                    'PPN Impor',
+                    'Value Added Tax'
+                ],
+                'PPh': [
+                    'PPh Impor',
+                    'Income Tax'
+                ],
+                'PPnBM': [
+                    'PPnBM Impor',
+                    'Luxury Goods Tax'
+                ],
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.newline {
+    white-space: pre-line;
+}
+</style>
--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <page-perhitungan-cd :data="data_perhitungan" />
+    <page-perhitungan-impor :data="data_perhitungan" />
   </div>
 </template>
 
@@ -18,6 +18,7 @@ import SelectJenisDokkap from "@/components/SelectJenisDokkap";
 import SelectJenisPungutan from "@/components/SelectJenisPungutan";
 
 import PagePerhitunganCd from "@/components/PagePerhitunganCd";
+import PagePerhitunganImpor from "@/components/PagePerhitunganImpor"
 
 const fileDownload = require("js-file-download");
 
@@ -30,56 +31,238 @@ export default {
     IpControls,
     LhpContents,
     SelectJenisPungutan,
-    PagePerhitunganCd
+    PagePerhitunganCd,
+    PagePerhitunganImpor
   },
 
   data() {
     return {
       data_perhitungan: {
-        pembebasan: 500,
-        ndpbm: 14253,
-        nilai_pembebasan_idr: 7126500,
-        nilai_dasar_idr: 1738879.3583549988,
-        nilai_impor: 1912879.3583549988,
-        tarif_bm: 10,
-        tarif_pph: 7.5,
-        pungutan: {
-          bm: 174000,
-          ppn: 192000,
-          pph: 144000
-        },
-        total: 510000,
         barang: [
           {
-            uraian:
-              "In veniam non omnis ut.\n20.20 KG\n------------------\nMAC ADDRESS : Blanditiis dignissimos numquam cumque dolor.\n",
-            jumlah_jenis_kemasan: "14.00 PK",
+            tarif: {
+              BM: {
+                tarif: 5,
+                jenis: "ADVALORUM"
+              },
+              PPN: {
+                tarif: 10
+              },
+              PPnBM: {
+                tarif: 40,
+                jenis: null,
+                bayar: 100,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0
+              },
+              PPh: {
+                tarif: 7.5
+              },
+              BMTP: {
+                tarif: 15,
+                jenis: "ADVALORUM",
+                bayar: 100,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0
+              }
+            },
+            pungutan: [
+              {
+                bayar: 5000,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0,
+                jenis_pungutan_id: 2,
+                jenis_pungutan: {
+                  id: 2,
+                  kode: "BM",
+                  nama: "Bea Masuk",
+                  kode_akun: "412111",
+                  created_at: "2020-07-07 17:40:52",
+                  updated_at: "2020-07-07 17:40:52"
+                }
+              },
+              {
+                bayar: 15000,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0,
+                jenis_pungutan_id: 5,
+                jenis_pungutan: {
+                  id: 5,
+                  kode: "BMTP",
+                  nama: "Bea Masuk Tindakan Pengamanan",
+                  kode_akun: "412123",
+                  created_at: "2020-07-07 17:40:52",
+                  updated_at: "2020-07-07 17:40:52"
+                }
+              },
+              {
+                bayar: 12000,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0,
+                jenis_pungutan_id: 8,
+                jenis_pungutan: {
+                  id: 8,
+                  kode: "PPN",
+                  nama: "PPN Impor",
+                  kode_akun: "411212",
+                  created_at: "2020-07-07 17:40:52",
+                  updated_at: "2020-07-07 17:40:52"
+                }
+              },
+              {
+                bayar: 48000,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0,
+                jenis_pungutan_id: 9,
+                jenis_pungutan: {
+                  id: 9,
+                  kode: "PPnBM",
+                  nama: "PPnBM Impor",
+                  kode_akun: "411222",
+                  created_at: "2020-07-07 17:40:53",
+                  updated_at: "2020-07-07 17:40:53"
+                }
+              },
+              {
+                bayar: 9000,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0,
+                jenis_pungutan_id: 7,
+                jenis_pungutan: {
+                  id: 7,
+                  kode: "PPh",
+                  nama: "PPh Impor",
+                  kode_akun: "411123",
+                  created_at: "2020-07-07 17:40:52",
+                  updated_at: "2020-07-07 17:40:52"
+                }
+              }
+            ],
+            uraian: "Est.\n22.81 KG",
+            jumlah_jenis_kemasan: "17.00 PK",
             jumlah_jenis_satuan: " ",
-            hs_code: "72202010",
-            hs_raw_code: "7220.20.10",
-            fob: 552.2087,
-            insurance: 56.1025,
-            freight: 24.8249,
-            cif: 633.1360999999999,
-            nilai_pabean: 6480496.208354999,
-            valuta: "SGD",
-            ndpbm: 10235.55
+            hs_code: "25174900",
+            hs_raw_code: "2517.49.00",
+            fob: 539.2227,
+            insurance: 92.5839,
+            freight: 107.8013,
+            cif: 739.6079,
+            nilai_pabean: 98494.61949406,
+            valuta: "JPY",
+            ndpbm: 133.1714
           },
           {
-            uraian: "Snake Venom\n5.00 KG",
-            jumlah_jenis_kemasan: "2.00 BX",
+            tarif: {
+              BM: {
+                tarif: 5,
+                jenis: "ADVALORUM"
+              },
+              PPN: {
+                tarif: 10
+              },
+              PPh: {
+                tarif: 7.5
+              }
+            },
+            pungutan: [
+              {
+                bayar: 5000,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0,
+                jenis_pungutan_id: 2,
+                jenis_pungutan: {
+                  id: 2,
+                  kode: "BM",
+                  nama: "Bea Masuk",
+                  kode_akun: "412111",
+                  created_at: "2020-07-07 17:40:52",
+                  updated_at: "2020-07-07 17:40:52"
+                }
+              },
+              {
+                bayar: 10000,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0,
+                jenis_pungutan_id: 8,
+                jenis_pungutan: {
+                  id: 8,
+                  kode: "PPN",
+                  nama: "PPN Impor",
+                  kode_akun: "411212",
+                  created_at: "2020-07-07 17:40:52",
+                  updated_at: "2020-07-07 17:40:52"
+                }
+              },
+              {
+                bayar: 8000,
+                bebas: 0,
+                tunda: 0,
+                tanggung_pemerintah: 0,
+                jenis_pungutan_id: 7,
+                jenis_pungutan: {
+                  id: 7,
+                  kode: "PPh",
+                  nama: "PPh Impor",
+                  kode_akun: "411123",
+                  created_at: "2020-07-07 17:40:52",
+                  updated_at: "2020-07-07 17:40:52"
+                }
+              }
+            ],
+            uraian:
+              "Minus ullam voluptate sed vitae est quis.\n0.76 KG\n------------------\nIMSI : Accusantium facilis adipisci itaque consectetur qui ut et voluptatem quas nesciunt fugit.\nIMSI : Sed non et cupiditate.\nIMSI : Quia voluptates sunt.\n",
+            jumlah_jenis_kemasan: "7.00 PK",
             jumlah_jenis_satuan: " ",
-            hs_code: "02109990",
-            hs_raw_code: "0210.99.90",
-            fob: 233,
-            insurance: 0,
-            freight: 0,
-            cif: 233,
-            nilai_pabean: 2384883.15,
-            valuta: "SGD",
-            ndpbm: 10235.55
+            hs_code: "38249910",
+            hs_raw_code: "3824.99.10",
+            fob: 561.0877,
+            insurance: 50.8508,
+            freight: 65.8356,
+            cif: 677.7741000000001,
+            nilai_pabean: 90260.12578074001,
+            valuta: "JPY",
+            ndpbm: 133.1714
           }
-        ]
+        ],
+        pungutan: {
+          bayar: {
+            BM: 10000,
+            BMTP: 15000,
+            PPN: 22000,
+            PPnBM: 48000,
+            PPh: 17000
+          },
+          bebas: {
+            BM: 0,
+            BMTP: 0,
+            PPN: 0,
+            PPnBM: 0,
+            PPh: 0
+          },
+          tunda: {
+            BM: 0,
+            BMTP: 0,
+            PPN: 0,
+            PPnBM: 0,
+            PPh: 0
+          },
+          tanggung_pemerintah: {
+            BM: 0,
+            BMTP: 0,
+            PPN: 0,
+            PPnBM: 0,
+            PPh: 0
+          }
+        }
       }
     };
   },
