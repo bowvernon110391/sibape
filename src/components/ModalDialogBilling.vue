931d27debf36e416c06dd15012037657e3623755
Bow Vernon <bowvernon110391@gmail.com>
2020-07-13 09:07:01 +0700

    Add ModalDialogBilling to input billing data

---
+++ src/components/ModalDialogBilling.vue
@@ -0,0 +1,67 @@
+<template>
+    <b-modal
+        v-on="$listeners"
+        v-bind="$attrs"
+        header-bg-variant="light"
+        footer-bg-variant="light"
+        ref="modal"
+    >
+        <!-- title -->
+        <template #modal-title>
+            Input Data Billing
+        </template>
+
+        <!-- body -->
+        <b-form-group label="Nomor Billing">
+            <b-form-input :v-model="nomor" :disabled="disabled"/>
+        </b-form-group>
+
+        <b-form-group label="Tanggal Billing">
+            <b-form-input :v-model="tanggal" :disabled="disabled"/>
+        </b-form-group>
+
+        <!-- footer -->
+        <template #modal-footer>
+            <b-button variant="primary" size="sm" :disabled="disabled" @click="handleOk">
+                <font-awesome-icon icon="save"/>
+                Simpan
+            </b-button>
+        </template>
+    </b-modal>
+</template>
+
+<script>
+import Datepicker from '@/components/Datepicker'
+
+export default {
+    inheritAttrs: false,
+
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    components: {
+        Datepicker
+    },
+
+    data () {
+        return {
+            nomor: '',
+            tanggal: ''
+        }
+    },
+
+    methods: {
+        handleOk() {
+            this.$refs['modal'].hide()
+            this.$emit('select', {
+                nomor: nomor,
+                tanggal: tanggal
+            })
+        }
+    }
+}
+</script>
